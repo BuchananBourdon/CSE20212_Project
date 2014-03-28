5990a1ebb7e7cc4f519d02e97e79c3affc333c70
Buck <bbourdon@nd.edu>
2014-03-28 03:18:52 -0400

    added functionality. select square, select icons, health bar via pressing shift and selected, right click to deselect. compile with g++ -o rabbit_animation rabbit_animation.cpp -lSDL -lSDL_image -lSDL_gfx

--- rabbit_animation.cpp
+++ rabbit_animation.cpp
@@ -2,8 +2,11 @@
 #include "SDL/SDL.h"
 #include <iostream>
 #include "SDL/SDL_image.h"
+#include "SDL/SDL_gfxPrimitives.h"
 #include <string>
+#include <vector>
 
+using namespace std;
 
 //Screen attributes
 const int SCREEN_WIDTH = 640;
@@ -13,6 +16,17 @@ const int SCREEN_BPP = 32;
 //Dimensions of the bunny
 const int BUNNY_WIDTH = 32;
 const int BUNNY_HEIGHT = 32;
+
+int xdown = 0;
+int ydown = 0;
+int xup = 0;
+int yup = 0;
+int moved = 0;
+int mousex = 0;
+int mousey = 0;
+
+const int FRAMES_PER_SECOND = 10;
+
 
 //The direction status of the figure
 const int FOO_RIGHT = 0;
@@ -24,7 +38,8 @@ using namespace std;
 //The surfaces
 SDL_Surface *bunny = NULL;
 SDL_Surface *screen = NULL;
-
+SDL_Surface *selected_circle = NULL;
+SDL_Surface *health = NULL;
 //The event structure
 SDL_Event event;
 
@@ -33,24 +48,25 @@ SDL_Rect clipsRight[ 4 ];
 SDL_Rect clipsLeft[ 4 ];
 SDL_Rect clipsUp[ 4 ];
 SDL_Rect clipsDown[ 4 ];
+SDL_Rect circle;
+SDL_Rect health_full;
 
-
-
-
-//The stick figure
+//The Bunny
 class Bunny
 {
+	friend void trackBunny(Bunny *);
+	friend int compareBunnyOffsets(int);
         public:
                	//Initializes the variables
-                Bunny();
+                Bunny(int = 0,int = SCREEN_HEIGHT - BUNNY_HEIGHT);
 
                 //Handles input
                 void handle_events();
 
-                //Moves the stick figure
+                //Moves the bunny
                 void move();
 
-                //Shows the stick figure
+                //Shows the bunny
                 void show();
 
         private:
@@ -70,8 +86,41 @@ class Bunny
 
                 int xgoal;
                 int ygoal;
+		int selected;
+		int showhealth;
 };
 
+
+class Timer
+{
+    private:
+    //The clock time when the timer started
+    int startTicks;
+
+    //The ticks stored when the timer was paused
+    int pausedTicks;
+
+    //The timer status
+    bool paused;
+    bool started;
+
+    public:
+    //Initializes variables
+    Timer();
+
+    //The various clock actions
+    void start();
+    void stop();
+    void pause();
+    void unpause();
+
+    //Gets the timer's time
+    int get_ticks();
+
+    //Checks the status of the timer
+    bool is_started();
+    bool is_paused();
+};
 
 
 
@@ -118,12 +167,12 @@ void set_clips()
     clipsLeft[ 3 ].w = BUNNY_WIDTH;
     clipsLeft[ 3 ].h = BUNNY_HEIGHT;
 
-	clipsUp[0].x = 0;
+	clipsUp[0].x = BUNNY_WIDTH * 0;
         clipsUp[0].y = BUNNY_HEIGHT * 2;
         clipsUp[0].w = BUNNY_WIDTH;
         clipsUp[0].h = BUNNY_HEIGHT;
 
-        clipsUp[1].x = BUNNY_WIDTH;
+        clipsUp[1].x = BUNNY_WIDTH * 1;
         clipsUp[1].y = BUNNY_HEIGHT * 2;
         clipsUp[1].w = BUNNY_WIDTH;
         clipsUp[1].h = BUNNY_HEIGHT;
@@ -133,10 +182,10 @@ void set_clips()
         clipsUp[2].w = BUNNY_WIDTH;
         clipsUp[2].h = BUNNY_HEIGHT;
 
-        clipsUp[2].x = BUNNY_WIDTH;
-        clipsUp[2].y = BUNNY_HEIGHT * 2;
-        clipsUp[2].w = BUNNY_WIDTH;
-     	clipsUp[2].h = BUNNY_HEIGHT;
+        clipsUp[3].x = BUNNY_WIDTH * 1;
+        clipsUp[3].y = BUNNY_HEIGHT * 2;
+        clipsUp[3].w = BUNNY_WIDTH;
+     	clipsUp[3].h = BUNNY_HEIGHT;
 
 
         clipsDown[0].x = 0;
@@ -159,7 +208,15 @@ void set_clips()
         clipsDown[3].w = BUNNY_WIDTH;
         clipsDown[3].h = BUNNY_HEIGHT;
 
+	circle.x = 0;
+	circle.y = 0;
+	circle.w = 80;
+	circle.h = 80;
 
+	health_full.x = 0;
+	health_full.y = 0;
+	health_full.w = 45;
+	health_full.h = 20;
 }
 
 
@@ -203,62 +260,128 @@ void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination,
 
 
 
-Bunny::Bunny()
+Bunny::Bunny(int x, int y)
 {
     //Initialize movement variables
-    offSet_x = 0;
-        offSet_y = SCREEN_HEIGHT - BUNNY_HEIGHT;
-    velocity_x = 0;
+    offSet_x = x;
+        //offSet_y = SCREEN_HEIGHT - BUNNY_HEIGHT;
+    	offSet_y = y;
+	velocity_x = 0;
         velocity_y = 0;
-
+	
     //Initialize animation variables
     frame = 0;
     status = FOO_RIGHT;
         xgoal = offSet_x;
         ygoal = offSet_y;
+	trackBunny(this);
+	selected = 0;
+	showhealth = 0;
 }
 
 void Bunny::handle_events()
 {
-
     //If a key was pressed
     if( event.type == SDL_MOUSEBUTTONDOWN )
     {
-        //Set the velocity
-        if( event.button.button == SDL_BUTTON_LEFT )
-        {
-                int x = 0;
-                int y = 0;
-                x = event.button.x;
-                xgoal = x;
-                y = event.button.y;
-                ygoal = y;
-            if(x > offSet_x)    velocity_x = BUNNY_WIDTH / 4;
-            if(x< offSet_x)     velocity_x = -(BUNNY_WIDTH / 4);
-                if(y < offSet_y+BUNNY_HEIGHT)   velocity_y = -(BUNNY_WIDTH /4);
-                if(y > offSet_y+BUNNY_HEIGHT)   velocity_y = BUNNY_WIDTH /4;
-        }
+	if(event.button.button == SDL_BUTTON_RIGHT)	selected = 0;
     }
+    else if(event.type == SDL_MOUSEBUTTONUP)
+    {
+	if(event.button.button == SDL_BUTTON_LEFT)
+	{
+		xup = event.button.x;
+		yup = event.button.y;
+	
+		int xleft,xright,ytop,ybottom;
+		if(xup < xdown)
+		{	
+			xleft = xup;
+			xright = xdown;
+		}
+		else if(xup > xdown)
+		{
+			xleft = xdown;
+			xright = xup;
+		}
+		if(yup < ydown)	
+		{
+			ytop = yup;
+			ybottom = ydown;
+		}
+		else if(yup > ydown)
+		{
+			ytop = ydown;
+			ybottom = yup;
+		}
+
+
+		if(xdown == xup && ydown == yup)
+		{
+			if(selected==1)
+			{
+				xgoal = xup;
+				ygoal = yup;
+				if(offSet_x < xgoal)    velocity_x = BUNNY_WIDTH / 2;
+                		if(xgoal < offSet_x)     velocity_x = -(BUNNY_WIDTH / 2);
+                		if(ygoal < offSet_y+BUNNY_HEIGHT)   velocity_y = -(BUNNY_WIDTH /2);
+                		if(ygoal > offSet_y+BUNNY_HEIGHT)   velocity_y = BUNNY_WIDTH /2;
+			}
+			selected = 0;
+		}
+		else
+		{
+			if(offSet_x+BUNNY_WIDTH/2 < xright && offSet_x+BUNNY_WIDTH/2 > xleft)
+			{
+				if(offSet_y+BUNNY_HEIGHT/2 > ytop && offSet_y+BUNNY_HEIGHT/2 < ybottom)
+				{
+					selected = 1;
+				}
+			}
+		}
+	}
+    }
+	if(event.type == SDL_KEYDOWN)
+	{
+		if(event.key.keysym.sym == SDLK_RSHIFT || event.key.keysym.sym == SDLK_LSHIFT)	
+		{
+			showhealth = 1;
+		}
+	}
+	if(event.type == SDL_KEYUP)
+	{
+		if(event.key.keysym.sym == SDLK_RSHIFT || event.key.keysym.sym == SDLK_LSHIFT)
+		{
+			showhealth = 0;
+		}
+	}
+
 }
 
 void Bunny::move()
 {
+        if(velocity_x>0 && offSet_x >= xgoal-BUNNY_WIDTH)	
+	{
+		velocity_x = 0;
+	}
+        if(velocity_x<0 && offSet_x <= xgoal)
+	{
+	   	velocity_x = 0;
+	}
+        if(velocity_y>0 && offSet_y >= ygoal-BUNNY_HEIGHT)	
+	{
+		velocity_y = 0;
+	}
+        if(velocity_y<0 && offSet_y <= ygoal)
+	{	
+		velocity_y = 0;
+	}
+    
 
-        if(velocity_x>0 && offSet_x >= xgoal-BUNNY_WIDTH)	velocity_x = 0;
-        if(velocity_x<0 && offSet_x <= xgoal)   velocity_x = 0;
-        if(velocity_y>0 && offSet_y >= ygoal-BUNNY_HEIGHT)	velocity_y = 0;
-        if(velocity_y<0 && offSet_y <= ygoal-BUNNY_HEIGHT)	velocity_y = 0;
-    //Move
-    offSet_x += velocity_x;
-        offSet_y += velocity_y;
+    		offSet_x += velocity_x;
+        	offSet_y += velocity_y;
 
-}
-
-
-void Bunny::show()
-{
-
-        if(velocity_x > 0 && velocity_y > 0)
+	if(velocity_x > 0 && velocity_y > 0)
         {
                 status = FOO_DOWN;
                 frame++;
@@ -268,24 +391,24 @@ void Bunny::show()
                 status = FOO_UP;
                 frame++;
         }
-	else if( velocity_x < 0 && velocity_y >0)
+        else if( velocity_x < 0 && velocity_y >0)
         {
                 status = FOO_DOWN;
                 frame++;
         }
-	else if(velocity_x < 0 && velocity_y <0)
+        else if(velocity_x < 0 && velocity_y <0)
         {
                 status = FOO_UP;
                 frame++;
         }
-	else if(velocity_x == 0 && velocity_y > 0)
+        else if(velocity_x == 0 && velocity_y > 0)
         {
                 status = FOO_DOWN;
                 frame++;
         }
         else if(velocity_x == 0 && velocity_y < 0)
         {
-                status = FOO_UP;
+               	status = FOO_UP;
                 frame++;
         }
 	else if(velocity_x >0 && velocity_y == 0)
@@ -293,13 +416,13 @@ void Bunny::show()
                 status = FOO_RIGHT;
                 frame++;
         }
-        else if(velocity_x < 0 && velocity_y == 0)
+	else if(velocity_x < 0 && velocity_y == 0)
         {
                 status = FOO_LEFT;
                 frame++;
         }
-        else
-        {
+	else
+	{
                 frame = 0;
         }
 
@@ -309,7 +432,14 @@ void Bunny::show()
      	frame = 0;
     }
 
-    //Show the stick figure
+	if(abs(offSet_x - xgoal) < 40 && abs(offSet_y - ygoal) < 40)	frame = 0;
+
+}
+
+
+void Bunny::show()
+{
+    //Show the bunny
     if( status == FOO_RIGHT )
     {
         apply_surface( offSet_x, offSet_y, bunny, screen, &clipsRight[ frame ] );
@@ -326,6 +456,136 @@ void Bunny::show()
     {
         apply_surface(offSet_x,offSet_y,bunny,screen,&clipsUp[ frame ] );
     }
+
+	if(selected==1)
+	{
+		apply_surface(offSet_x+BUNNY_WIDTH/2-40,offSet_y+BUNNY_HEIGHT/2-40,selected_circle,screen,&circle);
+		
+		if(showhealth==1)
+		{
+			apply_surface(offSet_x - 7,offSet_y+BUNNY_HEIGHT-3,health,screen,&health_full);
+		}
+		
+	}
+}
+
+
+vector<Bunny*> bunnyVec;
+
+void trackBunny(Bunny * rabbit)
+{
+        bunnyVec.push_back(rabbit);
+}
+
+
+int compareBunnyOffsets(int number)
+{
+	int i = bunnyVec.size();
+	for(int j = 0;j<1;j++)
+	{
+		if(abs((*bunnyVec[j]).offSet_x - (*bunnyVec[number]).offSet_x) < 40 && abs((*bunnyVec[j]).offSet_x - (*bunnyVec[j]).xgoal) < 60)
+		{
+			if(abs((*bunnyVec[j]).offSet_y - (*bunnyVec[number]).offSet_y) < 40 && abs((*bunnyVec[j]).offSet_y - (*bunnyVec[j]).ygoal) < 60)               
+                	{
+				return 0;
+			}
+		}
+	}
+
+	return 1;
+}
+
+
+Timer::Timer()
+{
+    //Initialize the variables
+    startTicks = 0;
+    pausedTicks = 0;
+    paused = false;
+    started = false;
+}
+
+void Timer::start()
+{
+    //Start the timer
+    started = true;
+
+    //Unpause the timer
+    paused = false;
+
+    //Get the current clock time
+    startTicks = SDL_GetTicks();
+}
+
+void Timer::stop()
+{
+    //Stop the timer
+    started = false;
+
+    //Unpause the timer
+    paused = false;
+}
+
+void Timer::pause()
+{
+    //If the timer is running and isn't already paused
+    if( ( started == true ) && ( paused == false ) )
+    {
+     	//Pause the timer
+        paused = true;
+
+        //Calculate the paused ticks
+        pausedTicks = SDL_GetTicks() - startTicks;
+    }
+}
+
+
+void Timer::unpause()
+{
+    //If the timer is paused
+    if( paused == true )
+    {
+     	//Unpause the timer
+        paused = false;
+
+        //Reset the starting ticks
+        startTicks = SDL_GetTicks() - pausedTicks;
+
+        //Reset the paused ticks
+        pausedTicks = 0;
+    }
+}
+
+int Timer::get_ticks()
+{
+    //If the timer is running
+    if( started == true )
+    {
+     	//If the timer is paused
+        if( paused == true )
+        {
+            //Return the number of ticks when the timer was paused
+            return pausedTicks;
+        }
+	else
+	{
+            //Return the current time minus the start time
+            return SDL_GetTicks() - startTicks;
+        }
+    }
+
+    //If the timer isn't running
+    return 0;
+}
+
+bool Timer::is_started()
+{
+    return started;
+}
+
+bool Timer::is_paused()
+{
+    return paused;
 }
 
 
@@ -364,6 +624,18 @@ bool load_files()
         return false;
     }
 
+	selected_circle = load_image("selected_circle.png");
+	if(selected_circle == NULL)
+	{
+		return false;
+	}
+	
+	health = load_image("health_full.png");
+	if(health == NULL)
+	{
+		return false;
+	}
+	
     //If everything loaded fine
     return true;
 }
@@ -373,75 +645,120 @@ void clean_up()
     //Free the surface
     SDL_FreeSurface( bunny );
 
+	SDL_FreeSurface(selected_circle);
+	SDL_FreeSurface(health);
+
     //Quit SDL
     SDL_Quit();
 }
 
 
+void setSelection()	//function that sets the global mouse input variables
+{
+	if( event.type == SDL_MOUSEBUTTONDOWN )
+    	{
+        	if(event.button.button == SDL_BUTTON_LEFT )
+        	{
+                	xdown = event.button.x;		//xdown and ydown are set if user left clicks
+                	ydown = event.button.y;
+			moved = 1;
+        	}
+	}
+
+	if(event.type == SDL_MOUSEMOTION)
+	{
+		mousex = event.motion.x;
+		mousey = event.motion.y;
+	}
+
+	if(event.type == SDL_MOUSEBUTTONUP)
+	{
+		if(event.button.button == SDL_BUTTON_LEFT)
+		{
+			moved = 0;
+		}
+	}
+}
+
+void highlightSelection()	//will draw a transparent highlight box
+{
+	if(moved==1)
+        {
+                boxRGBA(screen,xdown,ydown,mousex,mousey,60,60,255,170);
+        }
+}
 
 
 
 
 int main( int argc, char* args[] )
 {
-    //Quit flag
-    bool quit = false;
+    	bool quit = false; //quit flag
 
-    //Initialize
-    if( init() == false )
-    {
-        return 1;
-    }
+    	if( init() == false ) //Initialize
+    	{
+        	return 1;
+    	}
 
-    //Load the files
-    if( load_files() == false )
-    {
-        return 1;
-    }
+	Timer fps;	//instantiate the timer
+    	
+    	if( load_files() == false ) 	//Load the files
+    	{
+        	return 1;
+    	}
 
-    //Clip the sprite sheet
-    set_clips();
+ 	set_clips();     	//Clip the sprite sheet
 
-    //The stick figure
-    Bunny rabbit;
 
-    //While the user hasn't quit
-    while( quit == false )
-    {
+    	//Instantiate the minions
+    	Bunny rabbit(200,400);
+	Bunny rabbit2(300,100);
 
-        //While there's events to handle
-        while( SDL_PollEvent( &event ) )
-        {
-            //Handle events for the stick figure
-            rabbit.handle_events();
+    	while( quit == false )	//main while loop, runs until user X's out of screen
+    	{
+		fps.start();	//begin the timer
+        	SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) ); //clear the screen white
 
-            //If the user has Xed out the window
-            if( event.type == SDL_QUIT )
-            {
-                //Quit the program
-                quit = true;
-            }
-        }
+        	//While there's events to handle
+       		while( SDL_PollEvent( &event ) )
+        	{
+            		rabbit.handle_events();
+			rabbit2.handle_events();
+			setSelection();			//set user mouse input
+            		if( event.type == SDL_QUIT )        //If the user has Xed out the window
 
-        //Move the stick figure
-        rabbit.move();
+            		{
+                		quit = true;
+            		}
+        	}
+		
+		highlightSelection();	//draw the highlight square, will only work under appropriate conditions
 
-        //Fill the screen white
-        SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
+        	rabbit.move();
+		rabbit2.move();
+	
 
-        //Show the stick figure on the screen
-        rabbit.show();
+        	//Show the stick figures on the screen
+        	rabbit.show();
+		rabbit2.show();
 
-        //Update the screen
-        if( SDL_Flip( screen ) == -1 )
-        {
-            return 1;
-        }
+        	if( SDL_Flip( screen ) == -1 )         	//Update the screen
+
+        	{
+            		return 1;
+        	}
+
+
+
+		//Cap the frame rate
+        	if( fps.get_ticks() < 1000 / FRAMES_PER_SECOND )
+        	{
+            		SDL_Delay( ( 1000 / FRAMES_PER_SECOND ) - fps.get_ticks() );
+        	}
+
 	
     }
 
-    //Clean up
     clean_up();
-
     return 0;
 }
