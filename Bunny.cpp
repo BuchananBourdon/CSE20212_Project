c6c0e65c4de938390598102eae2fcbfcedb7f455
Buck <bbourdon@nd.edu>
2014-04-11 05:17:52 -0400

    Made new sprites and made bunnies resize appropriately

--- Bunny.cpp
+++ Bunny.cpp
@@ -5,20 +5,16 @@
 #include <cmath>
 
 SDL_Surface* Bunny::bunnySurface = NULL;
-SDL_Rect Bunny::clipsRight[ 4 ];
-SDL_Rect Bunny::clipsLeft[ 4 ];
-SDL_Rect Bunny::clipsUp[ 4 ];
-SDL_Rect Bunny::clipsDown[ 4 ];
+SDL_Rect Bunny::clipsRight[17][ 4 ];
+SDL_Rect Bunny::clipsLeft[17][ 4 ];
+SDL_Rect Bunny::clipsUp[17][ 4 ];
+SDL_Rect Bunny::clipsDown[17][ 4 ];
 	
-//Init these to 32, until we get image resize figured out
-int Bunny::BUNNY_WIDTH = 32;
-int Bunny::BUNNY_HEIGHT = 32;
-
 Bunny::Bunny(int _x, int _y) : Unit(_x,_y,1,1,100,DOWN,0)
 {
 	//If it's the first Bunny, then handle the image loading and clip dimensions
 	if(bunnySurface == NULL) {
-		SDL_Surface * loadedImage = IMG_Load("rabbit_animations.png");
+		SDL_Surface * loadedImage = IMG_Load("Bunny.png");
 		SDL_Surface * optimizedImage = SDL_DisplayFormatAlpha( loadedImage );
 		SDL_FreeSurface(loadedImage);
 		bunnySurface = optimizedImage;
@@ -32,15 +28,15 @@ int Bunny::getType() { return UT_BUNNY; }
 
 
 void Bunny::drawUnit(View & view) {
-
+	//blit the bunny to the screen if it's in view
 	SDL_Surface * screen = SDL_GetVideoSurface();
 	SDL_Rect rect;
 	rect.x = (x - view.x)*view.zoom;
 	rect.y = (y - view.y)*view.zoom;
-	if(status==RIGHT) 	SDL_BlitSurface(bunnySurface,&clipsRight[frame],screen,&rect);
-	else if(status == LEFT) SDL_BlitSurface(bunnySurface,&clipsLeft[frame],screen,&rect);
-	else if(status == UP) 	SDL_BlitSurface(bunnySurface,&clipsUp[frame],screen,&rect);
-	else if(status == DOWN)	SDL_BlitSurface(bunnySurface,&clipsDown[frame],screen,&rect);
+	if(status==RIGHT) 	SDL_BlitSurface(bunnySurface,&clipsRight[16-(((view.zoom+2)/6)-1)][frame],screen,&rect);
+	else if(status == LEFT) SDL_BlitSurface(bunnySurface,&clipsLeft[16-(((view.zoom+2)/6)-1)][frame],screen,&rect);
+	else if(status == UP) 	SDL_BlitSurface(bunnySurface,&clipsUp[16-(((view.zoom+2)/6)-1)][frame],screen,&rect);
+	else if(status == DOWN)	SDL_BlitSurface(bunnySurface,&clipsDown[16-(((view.zoom+2)/6)-1)][frame],screen,&rect);
 }
 
 //Virtual update method handles Bunny frame and status 
@@ -50,86 +46,29 @@ void Bunny::updateUnit(Map &map) {
 }
 
 void Bunny::setClips() {
-	
-	//Clip the sprites
-    	clipsRight[ 0 ].x = 0;
-    	clipsRight[ 0 ].y = 0;
-    	clipsRight[ 0 ].w = BUNNY_WIDTH;
-    	clipsRight[ 0 ].h = BUNNY_HEIGHT;
+	//Algorithm clips the Bunny sprite sheet with 17 different sizes
+	for(int i = 0;i<17;i++) {
+		for(int j = 0;j<4;j++) {
+			clipsRight[i][j].x = (100-i*6) * j;
+			clipsRight[i][j].y = 4*(880-(16-i)*(55-3*i));
+			clipsRight[i][j].h = 100-i*6;
+			clipsRight[i][j].w = j==1 ? 100-i*6-(16-i) : 100-i*6; 	
 
-    	clipsRight[ 1 ].x = BUNNY_WIDTH;
-    	clipsRight[ 1 ].y = 0;
-    	clipsRight[ 1 ].w = BUNNY_WIDTH;
-    	clipsRight[ 1 ].h = BUNNY_HEIGHT;
+			clipsLeft[i][j].x = (100-i*6) * j;
+                        clipsLeft[i][j].y = 4*(880-(16-i)*(55-3*i))+(100-6*i);
+                        clipsLeft[i][j].h = 100-i*6;
+                        clipsLeft[i][j].w = j==1 ? 100-i*6-(16-i) : 100-i*6; 	// Makes these clips just a few pixels less wide 
+										// because the images slightly overlap
 
-    	clipsRight[ 2 ].x = BUNNY_WIDTH * 2;
-    	clipsRight[ 2 ].y = 0;
-    	clipsRight[ 2 ].w = BUNNY_WIDTH;
-    	clipsRight[ 2 ].h = BUNNY_HEIGHT;
+			clipsUp[i][j].x = j<3 ? (100-i*6)*j : (100-i*6)*1; //4th frame is really just second frame again
+			clipsUp[i][j].y = 4*(880-(16-i)*(55-3*i)) + 2*(100-6*i);
+			clipsUp[i][j].w = 100-i*6;
+			clipsUp[i][j].h = 100-i*6;
 
-    	clipsRight[ 3 ].x = BUNNY_WIDTH * 3;
-    	clipsRight[ 3 ].y = 0;
-    	clipsRight[ 3 ].w = BUNNY_WIDTH;
-    	clipsRight[ 3 ].h = BUNNY_HEIGHT;
-
-    	clipsLeft[ 0 ].x = 0;
-    	clipsLeft[ 0 ].y = BUNNY_HEIGHT;
-    	clipsLeft[ 0 ].w = BUNNY_WIDTH;
-    	clipsLeft[ 0 ].h = BUNNY_HEIGHT;
-
-    	clipsLeft[ 1 ].x = BUNNY_WIDTH;
-    	clipsLeft[ 1 ].y = BUNNY_HEIGHT;
-    	clipsLeft[ 1 ].w = BUNNY_WIDTH;
-    	clipsLeft[ 1 ].h = BUNNY_HEIGHT;
-
-    	clipsLeft[ 2 ].x = BUNNY_WIDTH * 2;
- 	clipsLeft[ 2 ].y = BUNNY_HEIGHT;
-    	clipsLeft[ 2 ].w = BUNNY_WIDTH;
-    	clipsLeft[ 2 ].h = BUNNY_HEIGHT;
-
-    	clipsLeft[ 3 ].x = BUNNY_WIDTH * 3;
-    	clipsLeft[ 3 ].y = BUNNY_HEIGHT;
-    	clipsLeft[ 3 ].w = BUNNY_WIDTH;
-    	clipsLeft[ 3 ].h = BUNNY_HEIGHT;
-
-	clipsUp[0].x = BUNNY_WIDTH * 0;
-        clipsUp[0].y = BUNNY_HEIGHT * 2;
-        clipsUp[0].w = BUNNY_WIDTH;
-        clipsUp[0].h = BUNNY_HEIGHT;
-
-        clipsUp[1].x = BUNNY_WIDTH * 1;
-        clipsUp[1].y = BUNNY_HEIGHT * 2;
-        clipsUp[1].w = BUNNY_WIDTH;
-        clipsUp[1].h = BUNNY_HEIGHT;
-
-        clipsUp[2].x = BUNNY_WIDTH * 2;
-        clipsUp[2].y = BUNNY_HEIGHT * 2;
-        clipsUp[2].w = BUNNY_WIDTH;
-        clipsUp[2].h = BUNNY_HEIGHT;
-
-        clipsUp[3].x = BUNNY_WIDTH * 1;
-        clipsUp[3].y = BUNNY_HEIGHT * 2;
-        clipsUp[3].w = BUNNY_WIDTH;
-      	clipsUp[3].h = BUNNY_HEIGHT;
-
-
-        clipsDown[0].x = 0;
-        clipsDown[0].y = BUNNY_HEIGHT * 3;
-        clipsDown[0].w = BUNNY_WIDTH;
-        clipsDown[0].h = BUNNY_HEIGHT;
-
-        clipsDown[1].x = BUNNY_WIDTH;
-        clipsDown[1].y = BUNNY_HEIGHT * 3;
-        clipsDown[1].w = BUNNY_WIDTH;
-        clipsDown[1].h = BUNNY_HEIGHT;
-
-        clipsDown[2].x = BUNNY_WIDTH * 2;
-        clipsDown[2].y = BUNNY_HEIGHT * 3;
-        clipsDown[2].w = BUNNY_WIDTH;
-        clipsDown[2].h = BUNNY_HEIGHT;
-
-        clipsDown[3].x = BUNNY_WIDTH;
-        clipsDown[3].y = BUNNY_HEIGHT * 3;
-        clipsDown[3].w = BUNNY_WIDTH;
-        clipsDown[3].h = BUNNY_HEIGHT;			
+			clipsDown[i][j].x = j<3 ? (100-i*6)*j : (100-i*6)*1; //4th frame is really just second frame again
+			clipsDown[i][j].y = 4*(880-(16-i)*(55-3*i)) + 3*(100-6*i);
+			clipsDown[i][j].w = 100-i*6;
+			clipsDown[i][j].h = 100-i*6;
+		}
+	}
 }
